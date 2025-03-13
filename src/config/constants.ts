fa15bebc1faffe19c6bffa25d40056caec3202ef
Peter Ruß <peterruss1996@gmail.com>
2025-03-14 00:26:03 +0100

    feat(config): added general needed Site data as a constant

---
+++ src/config/constants.ts
@@ -0,0 +1,14 @@
+import { SiteConfig } from "@/types/SiteConfig";
+
+export const SITECONFIG: SiteConfig = {
+  name: "Peter-Russ-Blog",
+  url: "https://peter-russ.com",
+  description: "Peter Russ Blog - Software Development, Web Development, and more",
+  author: "Peter Russ",
+  links: {
+    github: "https://github.com/Peter-Russ",
+    xing: "https://www.xing.com/profile/Peter_Russ22/web_profiles",
+    linkedin: "https://www.linkedin.com/in/peter-ru%C3%9F-96a232245/",
+  }
+}
+
---
+++ src/types/SiteConfig.ts
@@ -0,0 +1,11 @@
+export type SiteConfig = {
+  name: string,
+  url: string,
+  description: string,
+  author: string,
+  links: {
+    github: string,
+    xing: string,
+    linkedin: string,
+  }
+}
