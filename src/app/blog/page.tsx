4789ec76a269f80fe47de0588cd4f4ae4d23db99
Peter Ruß <peterruss1996@gmail.com>
2025-03-21 23:20:05 +0100

    refactor: global css defined in styles folder

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blog",
+  description: "My latest blog posts",
+};
+
+export default function BlogPage() {
+  return (
+    <main>
+      <h1>Blog</h1>
+      {/* iterate through Blog posts */}
+    </main>
+  );
+}
